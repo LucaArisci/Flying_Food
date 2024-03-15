58734ebc399c50708bc72f237b36c5769cf137f1
LucArisci <luca.arisci91@gmail.com>
2024-03-15 15:04:53 +0100

    feat: adding dish component v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { CustomButtonComponent } from './shared/custom-button/custom-button.comp
 import { CircleButtonComponent } from './shared/circle-button/circle-button.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StdButtonComponent } from './shared/std-button/std-button.component';
+import { DishListComponent } from './dish-list/dish-list.component';
+import { DishComponent } from './dish-list/dish/dish.component';
 
 @NgModule({
-  declarations: [AppComponent, CustomButtonComponent, CircleButtonComponent, StdButtonComponent],
+  declarations: [AppComponent, CustomButtonComponent, CircleButtonComponent, StdButtonComponent, DishListComponent, DishComponent],
   imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
