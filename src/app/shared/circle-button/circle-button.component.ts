e19c51c3896ba947cf9893112cbf9eaacb8c2622
LucArisci <luca.arisci91@gmail.com>
2024-03-15 10:23:00 +0100

    feat: adding circle button component v1

---
+++ src/app/shared/circle-button/circle-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-button',
+  templateUrl: './circle-button.component.html',
+  styleUrl: './circle-button.component.css'
+})
+export class CircleButtonComponent {
+
+}
