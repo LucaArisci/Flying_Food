c349f52278dc65d0414d9d26f85e9fe054368b9e
LucArisci <luca.arisci91@gmail.com>
2024-03-15 09:47:42 +0100

    feat: implementing main component first grid structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { CustomButtonComponent } from './shared/custom-button/custom-button.component';
+import { CircleButtonComponent } from './shared/circle-button/circle-button.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CustomButtonComponent, CircleButtonComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
