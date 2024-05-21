a28f723ba406f90f30bcfb90c418574eda07e82a
LucArisci <luca.arisci91@gmail.com>
2024-05-21 11:46:57 +0200

    Feat: adding development environments

---
+++ src/environment/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  dev: true,
+  apiUrl: 'https://d4crsehc65.execute-api.eu-central-1.amazonaws.com/prod',
+  apiKey: 'e34vAOxj2C9fo2NVp9kT89QBOA4P1qvl459VtZak',
+};
---
+++ src/environment/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  dev: false,
+  apiUrl: 'http://localhost:4200',
+};
