e2c9fe6a78da951814a9d8a1492448d900deef51
LucArisci <luca.arisci91@gmail.com>
2024-05-21 11:57:48 +0200

    Feat: adding first profile component implementation

---
+++ src/app/sidebar/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+})
+export class AppProfileComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
