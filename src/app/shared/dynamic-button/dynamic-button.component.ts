cd1f933b63633c7868e60955d41ee80069ef98f2
LucArisci <luca.arisci91@gmail.com>
2024-05-21 11:58:36 +0200

    Feat: dynamic button functional implementation

--- src/app/shared/dynamic-button/dynamic-button.component.ts
+++ src/app/shared/dynamic-button/dynamic-button.component.ts
@@ -1,10 +1,48 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  Input,
+  OnInit,
+  TemplateRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-button',
   templateUrl: './dynamic-button.component.html',
   styleUrl: './dynamic-button.component.css',
 })
-export class DynamicButtonComponent {
+export class DynamicButtonComponent implements OnInit {
   @Input() btnType?: string;
+  @Input() btnText?: string;
+  @Input() btnIcon?: string;
+  @Input() btnClass?: string;
+  @Input() selected?: boolean;
+
+  private btnInitClass?: string;
+
+  ngOnInit(): void {
+    this.btnInitClass = this.btnClass;
+  }
+
+  toggleSelectionStandardBtn() {
+    this.toggleSelection();
+  }
+
+  toggleSelectionCustomBtn() {
+    this.toggleSelection();
+
+    this.btnClass = this.selected ? 'menu-selected' : 'menu-std';
+  }
+
+  toggleSelectionCircleBtn() {
+    this.toggleSelection();
+
+    this.btnClass =
+      this.btnClass === this.btnInitClass ? 'light-red' : this.btnInitClass;
+    this.btnIcon = this.btnIcon === 'add' ? 'done' : 'add';
+  }
+
+  toggleSelection() {
+    this.selected = !this.selected;
+  }
 }
